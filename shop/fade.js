d630eb38ffbdb81840b322f4dd27415a1264813b
Iconiclol <101067401+Iconiclol@users.noreply.github.com>
2024-09-21 16:10:05 -0500

    Update fade.js

--- shop/fade.js
+++ shop/fade.js
@@ -1,1 +1,10 @@
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const tsElement = document.querySelector('.floating-ts');
 
+        // Stop floating and trigger fade-out after 3 seconds
+        setTimeout(() => {
+            tsElement.style.animation = 'fadeOut 1s forwards'; // Trigger fade-out animation
+        }, 3000); // 3000 milliseconds = 3 seconds
+    });
+</script>
